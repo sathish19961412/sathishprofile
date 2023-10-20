22469122251ac22b88fd2c32dda1f0076c16c242
sathish19961412 <sathishfreelanc5@gmail.com>
2023-10-20 13:57:57 +0530

    banner section start completed

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Banner.css';
+import subimage from '../sub_image.png';
 import { Element } from 'react-scroll'
 
 function Banner() {
@@ -7,18 +8,27 @@ function Banner() {
     <Element name='about' className='about'>
           <section id="banner2">
                <div className='container'>
-                    <div className='row'>
+                    <div className='row' id="banner_section">
                         <div className='col-md-7'>
-                            <div className='d-flex justify-content-center' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
-                                <h3 className='text-center heading'>UI DESIGNER & DEVELOPER</h3>
+                            <div className='d-flex mt-5' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
+                                <span id="hello">Hello, I'm</span>
                             </div>
-                            <div className='d-flex justify-content-center' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
-                                <p className='text-justify' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">I fell in love with programming . I have at least learnt</p>
+                            <div className='d-flex' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
+                                <h1 id="name">Sathish</h1>
+                            </div>
+                            <div className='d-flex' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
+                                <p className='text-justify' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">A Creative <span className="designer">Web Designer</span> and  <span className="developer">Developer</span> From <span className="location">Coimbatore</span></p>
+                            </div>
+                            <div className='d-flex' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
+                                <span className='text-justify' id="slug" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">I'm creative designer based in New York, and I'm very passionate and dedicated to my work.</span>
+                            </div>
+                            <div className='d-flex mt-4' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
+                                 <a href="" className="about_button"><span>About Me</span></a>
                             </div>
                         </div>
                         <div className='col-md-5'>
                             <div className='d-flex justify-content-center' data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500" data-aos-once="true">
-                                {/* <h1 className='text-center heading'>{data[0].section2.title}</h1> */}
+                                 <img src={subimage} className='img-responsive' />
                             </div>
                         </div>
                     </div>
