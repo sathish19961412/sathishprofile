507d44d66af198d9ed6c06149ec39dad8f966c09
sathish19961412 <sathishfreelanc5@gmail.com>
2023-12-23 15:52:25 +0530

    portfolio add experience content in experience section

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../Experience/Experience.css'
 import {Element} from 'react-scroll'
 const Experience=()=>{
      return(
         <Element name='exp' className='exp'>
-             <section id='skils_set'>
+             <section id='experience'>
                  <div className='container'>
                     <div className='row'>
                          <div className='col-md-12'>
@@ -11,7 +12,31 @@ const Experience=()=>{
                                     <h1>OUR <b>Experience</b></h1>
                              </div>
                          </div>
-                        
+                         <div className='col-md-1'>&nbsp;</div>
+                         <div className='col-md-10'>
+                             <div className='d-flex justify-content-justify'>
+                                   <p>
+                                        We are Experience in UI Design and Developmenmt More than 2.5 years.<br></br> 20+ Website Developmenmt Successfully.
+
+                                   </p>
+                             </div>
+                             <div className='d-flex justify-content-justify'>
+                                   <p>
+                                        We are Experience in <span>Html,Css,Javascript,Bootstrap,Corephp,Mysql</span>
+                                   </p>
+                             </div>
+                             <div className='d-flex justify-content-justify'>
+                                   <p>
+                                        We are knowledge and Working To the Javascript-<span>Reactjs,angular,nodejs,expressjs,mongodb</span>
+                                   </p> 
+                             </div>
+                             <div className='d-flex justify-content-justify'>
+                                   <p>
+                                        We are knowledge and Working To the Python-<span>Python,Django,FastAPI</span>
+                                   </p>
+                             </div>
+                         </div>
+                         <div className='col-md-1'>&nbsp;</div>
                     </div>
                    
                  </div>
