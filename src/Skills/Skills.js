aecbbeec763ace77f8ce283992341d02f968907e
sathish19961412 <sathishfreelanc5@gmail.com>
2025-03-07 16:19:12 +0530

    update some content and images,animations

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,33 +1,24 @@
 import React from 'react'
 import {Element} from 'react-scroll'
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import '../assets/img/html.png'
+import '../assets/img/css.png'
+import '../assets/img/js.png'
+import '../assets/img/bootstrap.png'
+import '../assets/img/php.png'
+import '../assets/img/mysql.png'
+import '../assets/img/reactjs.png'
+import '../assets/img/nodejs.png'
+import '../assets/img/python.png'
+import '../assets/img/django.png'
+import '../assets/img/git.png'
+import '../assets/img/photoshop.jpg'
 import '../Skills/Skills.css'
 import '../assets/css/bootstrap.min.css'
 
 const Skills=()=>{
 
-     const now = 90;
-     const html='HTML';
-     const now1 = 85;
-     const css='CSS';
-     const js='Javascript';
-     const now2 =60;
-     const bootstrap='Bootstrap'
-     const now3=90;
-     const react='Reactjs'
-     const now4=50;
-     const php='Php';
-     const now5=60;
-     const now6=30;
-     const photoshop='Photoshop';
-     const mysql="Mysql";
-     const expressjs='Expressjs';
-     const nodejs='Nodejs';
-     const mongodb='Mongodb';
-     const python='Python';
-     const django='Django';
     
-
+   
      return(
         <Element name='skils' className='skils'>
             <section>
@@ -38,25 +29,73 @@ const Skills=()=>{
                                     <h1>OUR <b>Skills</b></h1>
                              </div>
                          </div>
-                    
-                         <div className='col-md-6'>
-                            <ProgressBar className='p--1' now={now} variant='success' label={` ${html} - ${now}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now1} variant='success' label={` ${css} - ${now1}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now2} variant="success" label={` ${js} - ${now2}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now3} variant="success" label={` ${bootstrap} - ${now3}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now4} variant="success" label={` ${react} - ${now4}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now5} variant="success" label={` ${php} - ${now5}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now6} variant="success" label={` ${photoshop} - ${now5}% `} /><br></br>
+                     </div>
+                     <div className='row'>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/html.png')} alt='html' className='img-fluid'/>
+                           </div>
                          </div>
-                         <div className='col-md-6'>
-                            <ProgressBar className='p--1' now={now5} variant="success" label={` ${mysql} - ${now5}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now6} variant="success" label={` ${nodejs} - ${now6}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now6} variant="success" label={` ${expressjs} - ${now6}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now6} variant="success" label={` ${mongodb} - ${now6}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now6} variant="success" label={` ${python} - ${now6}% `} /><br></br>
-                            <ProgressBar className='p--1' now={now6} variant="success" label={` ${django} - ${now6}% `} /><br></br>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/css.png')} alt='html' className='img-fluid'/>
+                           </div>
                          </div>
-                        
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/js.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/bootstrap.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                     </div>
+                     <div className='row mt-3'>
+                        <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/php.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/mysql.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/reactjs.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/nodejs.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                       
+                    </div>
+                    <div className='row mt-3'>
+                        <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/python.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/django.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/git.png')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
+                         <div className='col-md-3'>
+                           <div id='skill'>
+                              <img src={require('../assets/img/photoshop.jpg')} alt='html' className='img-fluid'/>
+                           </div>
+                         </div>
                     </div>
                  </div>
             </section>
